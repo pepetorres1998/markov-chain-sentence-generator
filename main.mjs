afec98f5ec4cd1a1f926cb78394a579bcdb1a1b2
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-07-04 02:47:21 -0500

    test for shitpost Markov

--- main.mjs
+++ main.mjs
@@ -1,10 +1,16 @@
 import fs from 'fs';
 import Markov from './markov.mjs';
+import ShitpostMarkov from './shitpost_markov.mjs'
 
 const file = fs.readFileSync('text.txt', 'utf8');
 
 var markov = new Markov(file.toString());
+var shitpostMarkov = new ShitpostMarkov(file.toString());
 
-console.log(markov.makeSentence(12));
+console.log(markov.makeEssay(4));
+console.log();
+console.log();
+console.log(shitpostMarkov.makeEssay(4));
+console.log();
 
 
